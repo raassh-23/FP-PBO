4b224d336bc792d92b1cff7c4067b222ef5b0a1e
Syamil28 <Syamil.sukur@gmail.com>
2020-12-03 00:09:07 +0900

    Project 2 Added

---
+++ Tugas Kelompok/Project 2/Syamil_Difaul_Haq_Sukur_05111940000196/src/id/ac/its/Syamil196/Project2/LabelTest.java
@@ -0,0 +1,15 @@
+package id.ac.its.Syamil196.Project2;
+
+// Fig. 12.7: LabelTest.java
+// Testing LabelFrame.
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+	public static void main(String[] args) {
+		LabelFrame labelFrame = new LabelFrame();
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(650, 650);
+		labelFrame.setVisible(true);
+	}
+} // end class LabelTest
