4aba64aa9ff10398369659a1356727c4b614f251
Muhammad Abdurrauf <muh.nur.abdurrauf@gmail.com>
2020-12-02 17:09:58 +0700

    add folders, add Shape and Rectangle in Project 1

---
+++ Tugas Kelompok/Project 1/src/id/ac/its/muhammadnurabdurrauf140/shapeapp/Rectangle.java
@@ -0,0 +1,43 @@
+package id.ac.its.muhammadnurabdurrauf140.shapeapp;
+
+public class Rectangle extends Shape {
+	
+	protected double length;
+	protected double width;
+	
+	public Rectangle() {
+		this.length = 0.0;
+		this.width = 0.0;
+	}
+	
+	public Rectangle(double length, double width) {
+		this.length = length;
+		this.width = width;
+	}
+
+	public double getLength() {
+		return length;
+	}
+	
+	public void setLength(double length) {
+		this.length = length;
+	}
+	
+	public double getWidth() {
+		return width;
+	}
+	
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	@Override
+	public double getArea() {
+		return this.length * this.width;
+	}
+	
+	@Override
+	public double getPerimeter() {
+		return 2 * (this.length + this.width);
+	}
+}
---
+++ Tugas Kelompok/Project 1/src/id/ac/its/muhammadnurabdurrauf140/shapeapp/Shape.java
@@ -0,0 +1,8 @@
+package id.ac.its.muhammadnurabdurrauf140.shapeapp;
+
+public abstract class Shape {
+
+	public abstract double getArea();
+	public abstract double getPerimeter();
+
+}
