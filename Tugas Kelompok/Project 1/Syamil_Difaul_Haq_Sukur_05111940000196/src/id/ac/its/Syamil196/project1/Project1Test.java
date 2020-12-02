c41e5f1e7e00d833e83b0758465b2f58f007645f
Syamil28 <Syamil.sukur@gmail.com>
2020-12-02 22:00:49 +0900

    Package Update

---
+++ Tugas Kelompok/Project 1/Syamil_Difaul_Haq_Sukur_05111940000196/src/id/ac/its/Syamil196/project1/Project1Test.java
@@ -0,0 +1,25 @@
+package id.ac.its.Syamil196.project1;
+
+import javax.swing.JOptionPane;
+
+public class Project1Test {
+
+	public static void main(String[] args) {
+		String inShape = JOptionPane.showInputDialog("Masukkan bidang yang diinginkan (Circle, ...)");
+ 
+		if(inShape.equals("Circle")) {
+			String inRadius = JOptionPane.showInputDialog("Masukkan Radius");
+			
+			float radius =  Float.parseFloat(inRadius);
+			
+			Circle crcl = new Circle(radius);
+			
+			String output = String.format("Luasnya adalah %.3f satuan persegi dan kelilingnya adalah %.3f satuan persegi", crcl.getArea(), crcl.getPerimeter());
+			
+			JOptionPane.showMessageDialog( null, output, "Luas dan keliling", JOptionPane.PLAIN_MESSAGE);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Input bidang salah");
+		}
+	}
+}
---
+++ Tugas Kelompok/Project 1/Syamil_Difaul_Haq_Sukur_05111940000196/src/id/ac/its/Syamil196/project1/Shape.java
@@ -0,0 +1,8 @@
+package id.ac.its.Syamil196.project1;
+
+public abstract class Shape {
+
+	public abstract double getArea();
+	public abstract double getPerimeter();
+
+}
