8f5e3ce9b8ced8012466beefac1f7360e80f017f
Muhammad Abdurrauf <muh.nur.abdurrauf@gmail.com>
2020-12-14 16:56:39 +0700

    Add resources, Sprite, and Alien

---
+++ Collision Detection/src/id/ac/its/rauf140/collisiondetection/Alien.java
@@ -0,0 +1,26 @@
+package id.ac.its.rauf140.collisiondetection;
+
+public class Alien extends Sprite{
+    private final int INITIAL_X = 400;
+
+    public Alien(int x, int y) {
+        super(x, y);
+
+        initAlien();
+    }
+
+    private void initAlien() {
+
+        loadImage("src/resources/alien.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
+        if (x < 0) {
+            x = INITIAL_X;
+        }
+
+        x -= 1;
+    }
+}
