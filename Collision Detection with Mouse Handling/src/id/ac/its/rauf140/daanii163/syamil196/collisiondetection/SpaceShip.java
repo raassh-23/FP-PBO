17d7b4500e3fab967c44d1aaa59bb0dc0ffa732c
daaniikusnanta <danynabilgk@gmail.com>
2020-12-16 19:25:15 +0700

    Add Collission Detection with Mouse Handling

---
+++ Collision Detection with Mouse Handling/src/id/ac/its/rauf140/daanii163/syamil196/collisiondetection/SpaceShip.java
@@ -0,0 +1,52 @@
+package id.ac.its.rauf140.daanii163.syamil196.collisiondetection;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpaceShip extends Sprite {
+    private int dx;
+    private int dy;
+    private List<Missile> missiles;
+
+    public SpaceShip(int x, int y) {
+        super(x, y);
+
+        initCraft();
+    }
+
+    private void initCraft() {
+        
+        missiles = new ArrayList<>();
+        loadImage("src/resources/craft.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
+        x = dx;
+        y = dy;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
+    }
+
+    public List<Missile> getMissiles() {
+        return missiles;
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    	dx = e.getX();
+    	dy = e.getY();
+    }
+}
+
