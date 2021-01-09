db605fcd89cfc3dd91f5a0360802ca62ad0e5e97
Syamil28 <Syamil.sukur@gmail.com>
2021-01-09 22:09:01 +0900

    Upload Basket Class

---
+++ Final Project/src/id/ac/its/rauf140/daanii163/syamil196/finalproject/Basket.java
@@ -0,0 +1,69 @@
+package id.ac.its.rauf140.daanii163.syamil196.finalproject;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket extends Sprite {
+    private int dx;
+    private int dy;
+    private int MAX_X;
+    private int MAX_Y;
+    private List<Missile> missiles;
+
+    public Basket(int x, int y) {
+        super(x, y);
+
+        initCraft();
+    }
+
+    private void initCraft() {
+        
+        missiles = new ArrayList<>();
+        loadImage("src/resources/craft.png");
+        getImageDimensions();
+        
+        this.MAX_X = 400 - getWidth();
+        this.MAX_Y = 300 - getHeight();
+    }
+
+    public void move() {
+
+        x = dx;
+        y = dy;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
+    }
+
+    public List<Missile> getMissiles() {
+        return missiles;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    	fire();
+    	
+    }
+    
+    public void mouseDragged(MouseEvent e) {
+    	fire();
+    	mouseMoved(e);
+    }
+    
+    public void mouseMoved(MouseEvent e) {
+    	if(e.getX() < MAX_X) dx = e.getX();
+    	if(e.getY() < MAX_Y) dy = e.getY();
+    }
+
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+
+}
+
